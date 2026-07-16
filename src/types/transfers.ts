a874e8f11ea29e07a534a559b3be68d1a7aaa175
maful <me@maful.web.id>
2026-07-16 11:54:59 +0700

    refactor: update Transfers type definition

--- src/types/transfers.ts
+++ src/types/transfers.ts
@@ -1,4 +1,5 @@
 import type { PaginationMeta } from './customers';
+import type { PaymentMethod } from "./payment_methods";
 
 export interface Transfer {
   id: string;
@@ -7,9 +8,11 @@ export interface Transfer {
   currency?: string;
   status: string;
   account_id?: string;
-  source_payment_method_id?: string;
-  destination_payment_method_id?: string;
+  source_payment_method: PaymentMethod | null;
+  destination_payment_method: PaymentMethod | null;
   description?: string;
+  seller_id: string | null;
+  reference: string | null;
   metadata?: Record<string, unknown>;
   created: number;
   updated: number;
@@ -28,5 +31,9 @@ export interface CreateTransferParams {
   source_payment_method_id?: string;
   destination_payment_method_id?: string;
   description?: string;
+  seller_id?: string;
+  confirm?: boolean;
+  sonar_session_id?: string;
+  reference?: string;
   metadata?: Record<string, unknown>;
 }
